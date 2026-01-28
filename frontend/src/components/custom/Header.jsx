21604b2bee33a00f5216af04c83199479d785e0a
“Deep <deepmandal9091@gmail.com>
2026-01-29 01:05:59 +0530

    add

--- frontend/src/components/custom/Header.jsx
+++ frontend/src/components/custom/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import { Button } from "../ui/button.jsx";
+import { Button } from "../ui/Button";
 import {
   Popover,
   PopoverContent,
