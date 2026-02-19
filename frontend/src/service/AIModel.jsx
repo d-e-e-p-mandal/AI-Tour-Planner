a01dd092855f1e07ef5be65c90defc1ba2fb5f02
Deep Mandal <deepmandal9091@gmail.com>
2026-02-20 01:21:54 +0530

    Update API URL to use environment variable

--- frontend/src/service/AIModel.jsx
+++ frontend/src/service/AIModel.jsx
@@ -1,9 +1,12 @@
 export async function generateTrip(prompt) {
-    const res = await fetch("http://localhost:3001/generate-trip", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ prompt }),
-    });
+   const res = await fetch(
+      `${import.meta.env.VITE_BACKEND_URL}/generate-trip`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ prompt }),
+      }
+    );
   
     if (!res.ok) throw new Error("AI request failed");
   
